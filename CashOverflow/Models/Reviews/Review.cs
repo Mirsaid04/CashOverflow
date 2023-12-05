2eb9ffd5599198e98df0a15570c3189469416cd8
Mirsaid04 <mirsaidd04@gmail.com>
2023-12-05 23:11:04 +0900

    CODE RUB: Make Code Clear

--- CashOverflow/Models/Reviews/Review.cs
+++ CashOverflow/Models/Reviews/Review.cs
@@ -12,7 +12,7 @@ namespace CashOverflow.Models.Reviews
     public class Review
     {
         public Guid id { get; set; }
-        public int Stars {  get; set; }
+        public int Stars { get; set; }
         public string Thoughts { get; set; }
         public Guid CompanyId { get; set; }
         [JsonIgnore]
